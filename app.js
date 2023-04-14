176b4388e90a5f462527c769c3c90f3a77ab47cc
ayan-de <deayan252@gmail.com>
2023-04-14 11:31:26 +0530

    "cors error"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require('./middlewares/Error')
